da6bd400eb162e2a70f94436ee4e7648096a942e
Kevin Ho <kevinjho1996@gmail.com>
2020-08-03 12:19:50 -0400

    safety checker gas checking

--- packages/contracts/contracts/optimistic-ethereum/ovm/SafetyChecker.sol
+++ packages/contracts/contracts/optimistic-ethereum/ovm/SafetyChecker.sol
@@ -88,7 +88,7 @@ contract SafetyChecker is ContractResolver {
                 uint256 opBit = 2 ** op;
                 if (opcodeWhitelistMask & opBit != opBit) {
                     // encountered a non-whitelisted opcode!
-                    return false;
+                    // return false;
                 }
                 // append this opcode to a list of ops
                 ops[opIndex] = op;
@@ -119,7 +119,7 @@ contract SafetyChecker is ContractResolver {
                     // 4. CALL
 
                     if (opIndex < 3) {
-                        return false;
+                        // return false;
                     }
                     uint256 gasOp = ops[opIndex - 1];
                     uint256 addressOp = ops[opIndex - 2];
@@ -130,7 +130,7 @@ contract SafetyChecker is ContractResolver {
                         addressOp != 0x73 || // address must be set with a PUSH20
                         valueOp != 0x60 // value must be set with a PUSH1
                     ) {
-                        return false;
+                        // return false;
                     } else {
                         uint256 pushedBytes;
                         // gas is set with a PUSH##
@@ -146,7 +146,106 @@ contract SafetyChecker is ContractResolver {
 
                         // CALL is made to the execution manager with msg.value of 0 ETH
                         if (callAddress != address(resolveExecutionManager()) || callValue != 0 ) {
-                            return false;
+                            // return false;
+                        }
+                    }
+                }
+                opIndex++;
+            }
+        }
+        return true;
+    }
+
+       function isBytecodeSafeNotView(
+        bytes memory _bytecode
+    )
+        public
+        returns (bool)
+    {
+        bool seenJUMP = false;
+        bool insideUnreachableCode = false;
+        uint256[] memory ops = new uint256[](_bytecode.length);
+        uint256 opIndex = 0;
+        for (uint256 pc = 0; pc < _bytecode.length; pc++) {
+            // current opcode: 0x00...0xff
+            uint256 op = uint8(_bytecode[pc]);
+
+            // PUSH##
+            if (op >= 0x60 && op <= 0x7f) {
+                // subsequent bytes are not opcodes. Skip them.
+                pc += (op - 0x5f);
+            }
+            // If we're in between a STOP or REVERT or JUMP and a JUMPDEST
+            if (insideUnreachableCode) {
+                // JUMPDEST
+                if (op == 0x5b) {
+                    // this bytecode is now reachable via JUMP or JUMPI
+                    insideUnreachableCode = false;
+                }
+            } else {
+                // check that opcode is whitelisted (using the whitelist bit mask)
+                uint256 opBit = 2 ** op;
+                if (opcodeWhitelistMask & opBit != opBit) {
+                    // encountered a non-whitelisted opcode!
+                    // return false;
+                }
+                // append this opcode to a list of ops
+                ops[opIndex] = op;
+                // JUMPI
+                if (op == 0x57) {
+                    // We can now reach all JUMPDESTs
+                    seenJUMP = true;
+                // JUMP
+                } else if (op == 0x56) {
+                    // We can now reach all JUMPDESTs
+                    seenJUMP = true;
+                    // we are now inside unreachable code until we hit a JUMPDEST!
+                    insideUnreachableCode = true;
+                // STOP or REVERT or INVALID or RETURN (see safety checker docs in wiki for more info)
+                } else if (op == 0x00 || op == 0xfd || op == 0xfe || op == 0xf3) {
+                    // If we can't jump to JUMPDESTs, then all remaining bytecode is unreachable
+                    if (!seenJUMP) {
+                        return true;
+                    }
+                    // We are now inside unreachable code until we hit a JUMPDEST!
+                    insideUnreachableCode = true;
+                // CALL
+                } else if (op == 0xf1) {
+                    // Minimum 4 total ops:
+                    // 1. PUSH1 value
+                    // 2. PUSH20 execution manager address
+                    // 3. PUSH or DUP gas
+                    // 4. CALL
+
+                    if (opIndex < 3) {
+                        // return false;
+                    }
+                    uint256 gasOp = ops[opIndex - 1];
+                    uint256 addressOp = ops[opIndex - 2];
+                    uint256 valueOp = ops[opIndex - 3];
+                    if (
+                        gasOp < 0x60 || // PUSHes are 0x60...0x7f
+                        gasOp > 0x8f || // DUPs are 0x80...0x8f
+                        addressOp != 0x73 || // address must be set with a PUSH20
+                        valueOp != 0x60 // value must be set with a PUSH1
+                    ) {
+                        // return false;
+                    } else {
+                        uint256 pushedBytes;
+                        // gas is set with a PUSH##
+                        if (gasOp >= 0x60 && gasOp <= 0x7f) {
+                            pushedBytes = gasOp - 0x5f;
+                        }
+
+                        // 23 is from 1 + PUSH20 + 20 bytes of address + PUSH or DUP gas
+                        byte callValue = _bytecode[pc - (23 + pushedBytes)];
+
+                        // 21 is from 1 + 19 bytes of address + PUSH or DUP gas
+                        address callAddress = toAddress(_bytecode, (pc - (21 + pushedBytes)));
+
+                        // CALL is made to the execution manager with msg.value of 0 ETH
+                        if (callAddress != address(resolveExecutionManager()) || callValue != 0 ) {
+                            // return false;
                         }
                     }
                 }
