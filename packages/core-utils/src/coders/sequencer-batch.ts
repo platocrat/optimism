e2de641ea4dddea584f2a0facdd7ab101251aaac
Kelvin Fichter <kelvinfichter@gmail.com>
2021-02-10 00:35:29 -0500

    Clean up appendSequencerBatch coder

--- packages/core-utils/src/coders/sequencer-batch.ts
+++ packages/core-utils/src/coders/sequencer-batch.ts
@@ -1,7 +1,5 @@
 import { add0x, remove0x, encodeHex } from '../utils'
-import { Contract, BigNumber } from 'ethers'
-import { keccak256 } from 'ethers/lib/utils'
-import { TransactionResponse } from '@ethersproject/abstract-provider'
+import { BigNumber } from 'ethers'
 
 export interface BatchContext {
   numSequencedTransactions: number
@@ -17,84 +15,120 @@ export interface AppendSequencerBatchParams {
   transactions: string[] // total_size_bytes[],total_size_bytes[]
 }
 
-/**********************
- * Internal Functions *
- *********************/
-
-const APPEND_SEQUENCER_BATCH_METHOD_ID = 'appendSequencerBatch()'
-
-const appendSequencerBatch = async (
-  OVM_CanonicalTransactionChain: Contract,
-  batch: AppendSequencerBatchParams
-): Promise<TransactionResponse> => {
-  const methodId = keccak256(
-    Buffer.from(APPEND_SEQUENCER_BATCH_METHOD_ID)
-  ).slice(2, 10)
-  const calldata = encodeAppendSequencerBatch(batch)
-  return OVM_CanonicalTransactionChain.signer.sendTransaction({
-    to: OVM_CanonicalTransactionChain.address,
-    data: '0x' + methodId + calldata,
-  })
-}
-
+/**
+ * Encodes the calldata necessary to make a call to `appendSequencerBatch`.
+ * @param batchParams Parameters to the function call.
+ * @return Encoded calldata for the function call. Currently does *not* include the sighash.
+ */
 export const encodeAppendSequencerBatch = (
-  b: AppendSequencerBatchParams
+  batchParams: AppendSequencerBatchParams
 ): string => {
-  const encodedShouldStartAtBatch = encodeHex(b.shouldStartAtBatch, 10)
-  const encodedTotalElementsToAppend = encodeHex(b.totalElementsToAppend, 6)
+  // TODO: Should we include the function signature here?
 
-  const encodedContextsHeader = encodeHex(b.contexts.length, 6)
-  const encodedContexts =
-    encodedContextsHeader +
-    b.contexts.reduce((acc, cur) => acc + encodeBatchContext(cur), '')
+  // (5 bytes) index of the element we expect to append
+  const encodedShouldStartAtBatch = encodeHex(
+    batchParams.shouldStartAtBatch,
+    10
+  )
 
-  const encodedTransactionData = b.transactions.reduce((acc, cur) => {
-    if (cur.length % 2 !== 0) {
-      throw new Error('Unexpected uneven hex string value!')
-    }
-    const encodedTxDataHeader = remove0x(
-      BigNumber.from(remove0x(cur).length / 2).toHexString()
-    ).padStart(6, '0')
-    return acc + encodedTxDataHeader + remove0x(cur)
-  }, '')
+  // (3 bytes) total number of elements to append
+  const encodedTotalElementsToAppend = encodeHex(
+    batchParams.totalElementsToAppend,
+    6
+  )
+
+  // (3 bytes) total number of contexts
+  const encodedContextsHeader = encodeHex(batchParams.contexts.length, 6)
+
+  // (? bytes) a series of 16 byte "batch contexts." Each batch context is exactly ** 16 bytes **.
+  // The number of contexts comes from the 3 bytes above. Each context has the following structure:
+  //    - (3 bytes) number of sequencer transactions that will utilize this batch context.
+  //    - (3 bytes) number of queue transactions that will be inserted into the chain after the
+  //      the sequencer transactions.
+  //    - (5 bytes) timestamp that will be assigned to the sequencer transactions.
+  //    - (5 bytes) block number that will be assigned to the sequencer transactions.
+  const encodedContexts = batchParams.contexts
+    .map((context) => {
+      return (
+        encodeHex(context.numSequencedTransactions, 6) +
+        encodeHex(context.numSubsequentQueueTransactions, 6) +
+        encodeHex(context.timestamp, 10) +
+        encodeHex(context.blockNumber, 10)
+      )
+    })
+    .join('')
+
+  // (? bytes) a series of dynamically sized transaction data chunks. Each transaction consists of
+  // the following information:
+  //    - (3 bytes) total size of the coming transaction data in bytes.
+  //    - (? bytes) arbitrary data of length equal to that described by the first three bytes.
+  const encodedTransactionData = batchParams.transactions
+    .map((transaction) => {
+      if (transaction.length % 2 !== 0) {
+        throw new Error('Unexpected uneven hex string value!')
+      }
+
+      const encodedTransactionDataHeader = remove0x(
+        BigNumber.from(remove0x(transaction).length / 2).toHexString()
+      ).padStart(6, '0')
+
+      return encodedTransactionDataHeader + remove0x(transaction)
+    })
+    .join('')
+
+  // Join it all up.
   return (
     encodedShouldStartAtBatch +
     encodedTotalElementsToAppend +
+    encodedContextsHeader +
     encodedContexts +
     encodedTransactionData
   )
 }
 
-const encodeBatchContext = (context: BatchContext): string => {
-  return (
-    encodeHex(context.numSequencedTransactions, 6) +
-    encodeHex(context.numSubsequentQueueTransactions, 6) +
-    encodeHex(context.timestamp, 10) +
-    encodeHex(context.blockNumber, 10)
-  )
-}
-
 export const decodeAppendSequencerBatch = (
-  b: string
+  encodedSequencerBatch: string
 ): AppendSequencerBatchParams => {
-  b = remove0x(b)
+  encodedSequencerBatch = remove0x(encodedSequencerBatch)
 
-  const shouldStartAtBatch = b.slice(0, 10)
-  const totalElementsToAppend = b.slice(10, 16)
-  const contextHeader = b.slice(16, 22)
+  // (5 bytes) index of the element we expect to append
+  const shouldStartAtBatch = encodedSequencerBatch.slice(0, 10)
+
+  // (3 bytes) total number of elements to append
+  const totalElementsToAppend = encodedSequencerBatch.slice(10, 16)
+
+  // (3 bytes) total number of contexts
+  const contextHeader = encodedSequencerBatch.slice(16, 22)
   const contextCount = parseInt(contextHeader, 16)
 
+  // Scanning by offset. Initial offset is 5 + 3 + 3 bytes = 22 characters.
   let offset = 22
+
   const contexts = []
   for (let i = 0; i < contextCount; i++) {
-    const numSequencedTransactions = b.slice(offset, offset + 6)
+    // (3 bytes)
+    const numSequencedTransactions = encodedSequencerBatch.slice(
+      offset,
+      offset + 6
+    )
     offset += 6
-    const numSubsequentQueueTransactions = b.slice(offset, offset + 6)
+
+    // (3 bytes)
+    const numSubsequentQueueTransactions = encodedSequencerBatch.slice(
+      offset,
+      offset + 6
+    )
     offset += 6
-    const timestamp = b.slice(offset, offset + 10)
+
+    // (5 bytes)
+    const timestamp = encodedSequencerBatch.slice(offset, offset + 10)
     offset += 10
-    const blockNumber = b.slice(offset, offset + 10)
+
+    // (5 bytes)
+    const blockNumber = encodedSequencerBatch.slice(offset, offset + 10)
     offset += 10
+
+    // Make it pretty.
     contexts.push({
       numSequencedTransactions: parseInt(numSequencedTransactions, 16),
       numSubsequentQueueTransactions: parseInt(
@@ -109,11 +143,19 @@ export const decodeAppendSequencerBatch = (
   const transactions = []
   for (const context of contexts) {
     for (let i = 0; i < context.numSequencedTransactions; i++) {
-      const size = b.slice(offset, offset + 6)
+      // (3 bytes)
+      const size = encodedSequencerBatch.slice(offset, offset + 6)
       offset += 6
-      const raw = b.slice(offset, offset + parseInt(size, 16) * 2)
-      transactions.push(add0x(raw))
+
+      // (`size` bytes)
+      const raw = encodedSequencerBatch.slice(
+        offset,
+        offset + parseInt(size, 16) * 2
+      )
       offset += raw.length
+
+      // Make it pretty.
+      transactions.push(add0x(raw))
     }
   }
 
