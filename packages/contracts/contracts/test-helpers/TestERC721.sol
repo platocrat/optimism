a35b419b7771cb743a6bb6bebbac2b08e5f32ebf
platocrat <jack.abe.winfield@gmail.com>
2021-08-04 17:01:22 -0500

    start of draft to add ERC721

---
+++ packages/contracts/contracts/test-helpers/TestERC721.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.5.0 <0.8.0;
+
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/EnumerableMap.sol";
+
+// a test ERC721 token with an open mint function
+contract TestERC721 {
+    using SafeMath for uint;
+    using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    string public constant name = 'Test';
+    string public constant symbol = 'TST';
+    uint256 public totalSupply;
+
+    mapping(address => uint) public balances;
+    mapping (uint256 => address) private _tokenApprovals;
+    mapping (address => mapping (address => bool)) private _operatorApprovals;
+    mapping (address => EnumerableSet.UintSet) private _holderTokens;
+
+    EnumerableMap.UintToAddressMap private _tokenOwners;
+
+    constructor() {}
+
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _tokenOwners.contains(tokenId);
+    }
+
+    function _mint(address to, uint256 tokenId) public {
+        require(!_exists(tokenId), "ERC721: token already minted");
+        _holderTokens[to].add(tokenId);
+        _tokenOwners.set(tokenId, to);
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _approve(address to, uint256 tokenId) private {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ERC721.ownerOf(tokenId), to, tokenId); // internal owner
+    }
+
+
+    function _transfer(address from, address to, uint256 tokenId) internal virtual {
+        require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer of token that is not own"); // internal owner
+        _beforeTokenTransfer(from, to, tokenId);
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+        _holderTokens[from].remove(tokenId);
+        _holderTokens[to].add(tokenId);
+        _tokenOwners.set(tokenId, to);
+        emit Transfer(from, to, tokenId);
+    }
+
+    function approve(address to, uint256 tokenId) external returns (bool) {
+        _approve(to, tokenId);
+        return true;
+    }
+
+    function transfer(address to, uint256 tokenId) external returns (bool) {
+        _transfer(to, tokenId);
+        return true;
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
+        address owner = ERC721.ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || ERC721.isApprovedForAll(owner, spender));
+    }
+
+    function isApprovedForAll(address owner, address operator) public view virtual returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function getApproved(uint256 tokenId) public view virtual returns (address) {
+        require(_exists(tokenId), "ERC721: approved query for nonexistent token");
+        return _tokenApprovals[tokenId];
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) external returns (bool) {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+        _transfer(from, to, tokenId);
+        return true;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual { }
+}
+
+
+library SafeMath {
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+}
