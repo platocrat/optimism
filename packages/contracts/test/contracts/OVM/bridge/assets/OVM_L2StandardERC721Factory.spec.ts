aa9f17b3bcd89b04e8694fa3448c5220630854f3
Francesco Ceccon <francesco@ceccon.me>
2021-08-06 17:47:07 +0100

    feat: add L2 ERC721 token factory

--- packages/contracts/test/contracts/OVM/bridge/assets/OVM_L2StandardERC721Factory.spec.ts
+++ packages/contracts/test/contracts/OVM/bridge/assets/OVM_L2StandardERC721Factory.spec.ts
@@ -8,57 +8,57 @@ import { smoddit } from '@eth-optimism/smock'
 /* Internal Imports */
 import { getContractInterface, predeploys } from '../../../../../src'
 
-describe('OVM_L2StandardTokenFactory', () => {
+describe('OVM_L2StandardERC721Factory', () => {
   let signer: Signer
-  let Factory__L1ERC20: ContractFactory
-  let L1ERC20: Contract
-  let OVM_L2StandardTokenFactory: Contract
+  let Factory__L1ERC721: ContractFactory
+  let L1ERC721: Contract
+  let OVM_L2StandardERC721Factory: Contract
   before(async () => {
     ;[signer] = await ethers.getSigners()
     // deploy an ERC20 contract on L1
-    Factory__L1ERC20 = await smoddit(
-      '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20'
+    Factory__L1ERC721 = await smoddit(
+      '@openzeppelin/contracts/token/ERC721/ERC721.sol:ERC721'
     )
-    L1ERC20 = await Factory__L1ERC20.deploy('L1ERC20', 'ERC')
+    L1ERC721 = await Factory__L1ERC721.deploy('L1ERC721', 'NFT')
 
-    OVM_L2StandardTokenFactory = await (
-      await ethers.getContractFactory('OVM_L2StandardTokenFactory')
+    OVM_L2StandardERC721Factory = await (
+      await ethers.getContractFactory('OVM_L2StandardERC721Factory')
     ).deploy()
   })
 
-  describe('Standard token factory', () => {
-    it('should be able to create a standard token', async () => {
-      const tx = await OVM_L2StandardTokenFactory.createStandardL2Token(
-        L1ERC20.address,
-        'L2ERC20',
-        'ERC'
+  describe('Standard ERC721 token factory', () => {
+    it('should be able to create a standard ERC721 token', async () => {
+      const tx = await OVM_L2StandardERC721Factory.createStandardL2ERC721(
+        L1ERC721.address,
+        'L2ERC721',
+        'NFT'
       )
       const receipt = await tx.wait()
       const [tokenCreatedEvent] = receipt.events
 
       // Expect there to be an event emitted for the standard token creation
-      expect(tokenCreatedEvent.event).to.be.eq('StandardL2TokenCreated')
+      expect(tokenCreatedEvent.event).to.be.eq('StandardL2ERC721Created')
 
       // Get the L2 token address from the emitted event and check it was created correctly
       const l2TokenAddress = tokenCreatedEvent.args._l2Token
       const l2Token = new Contract(
         l2TokenAddress,
-        getContractInterface('L2StandardERC20'),
+        getContractInterface('L2StandardERC721'),
         signer
       )
 
       expect(await l2Token.l2Bridge()).to.equal(predeploys.OVM_L2StandardBridge)
-      expect(await l2Token.l1Token()).to.equal(L1ERC20.address)
-      expect(await l2Token.name()).to.equal('L2ERC20')
-      expect(await l2Token.symbol()).to.equal('ERC')
+      expect(await l2Token.l1Token()).to.equal(L1ERC721.address)
+      expect(await l2Token.name()).to.equal('L2ERC721')
+      expect(await l2Token.symbol()).to.equal('NFT')
     })
 
     it('should not be able to create a standard token with a 0 address for l1 token', async () => {
       await expect(
-        OVM_L2StandardTokenFactory.createStandardL2Token(
+        OVM_L2StandardERC721Factory.createStandardL2ERC721(
           ethers.constants.AddressZero,
-          'L2ERC20',
-          'ERC'
+          'L2ERC721',
+          'NFT'
         )
       ).to.be.revertedWith('Must provide L1 token address')
     })
